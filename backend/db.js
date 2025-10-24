15878f90ff546c9de9ce89d2139898b895979b41
ravitejamgit <mraviteja424@gmail.com>
2025-10-24 22:08:20 +0530

    Fully functioning app with localstorage..

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,24 @@
-const mysql = require('mysql2');
+require('dotenv').config()
+const mysql = require('mysql2/promise')
 
-const db_connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'task_manager'
-});
 
-db_connection.connect((error)=>{
-    if(error) {
-        console.log("Failed to conned to database..");
-        return;
-    }
-    console.log("Connected to Database....");
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
-module.exports = db_connection;
+async function getData() {
+    try {
+        const [data] = await db.query('select * from data');
+        console.log(data);
+    }
+    catch(error) {
+        console.log('error in fetching data', error);
+    }
+}
+
+
+getData();
+db.end(() => {console.log('Connections ended.')});
